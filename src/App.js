4ed0bebe7f55d5ed41d144a4a7f7144584310a82
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-05-27 02:40:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+
+  \`\`\`
+  {
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `);
+
+  marked.setOptions({
+      breaks: true,
+  });
+
+  const typeFx = (event) => {
+      setText(event.target.value);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div id='App'>
+     
+        <textarea id='editor'onChange={typeFx} defaultValue={text}></textarea>
+        <div id='preview' dangerouslySetInnerHTML={{__html: marked.parse(text)}}></div>
+      </div>
+  )
+
+};
+
 
 export default App;
